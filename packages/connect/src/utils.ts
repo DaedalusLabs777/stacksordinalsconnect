7d3de96b8dd275ae2bd683c6df513fd586a9ac2f
Richard Lu <yubo.richard.lu@gmail.com>
2023-10-30 22:45:38 -0700

    fix(ui): hiro checker

--- packages/connect/src/utils.ts
+++ packages/connect/src/utils.ts
@@ -1,7 +1,23 @@
 export function getStacksProvider() {
+  //console.log('StacksProvider:', window.StacksProvider); // Log the value of StacksProvider
+  //console.log('BlockstackProvider:', window.BlockstackProvider); // Log the value of BlockstackProvider
   return window.StacksProvider || window.BlockstackProvider;
+  }
+  
+  
+  export function isStacksWalletInstalled() {
+  const provider = getStacksProvider() as any;
+  
+  
+  console.log('test', !provider.isHiroWallet); // Log the value of StacksProvider
+  // If the provider exists and isHiroWallet is either not true or undefined, return true.
+  if (provider && (!provider.isHiroWallet || provider.isHiroWallet === undefined)) {
+  return true;
+  }
+  
+  
+  // Otherwise, return false.
+  return false;
 }
 
-export function isStacksWalletInstalled() {
-  return !!getStacksProvider();
-}
+  
